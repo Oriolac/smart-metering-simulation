8b5f3beb9376be700115a02077679615f3006ecb
Oriolac <oriolalascercos@gmail.com>
2020-02-17 19:10:18 +0100

    Added all docs of crypt. Needs to be reviewed.

--- src/udl/cig/sms/crypt/Cypher.java
+++ src/udl/cig/sms/crypt/Cypher.java
@@ -4,9 +4,19 @@ import cat.udl.cig.ecc.GeneralECPoint;
 
 import java.math.BigInteger;
 
+/**
+ * Cypher Interface.
+ */
 public interface Cypher {
 
+    /**
+     * Given a message and a big Integer t, it encripts the message
+     * using the method described in the papers : TODO put reference.
+     *
+     * @param message to be encrypted
+     * @param t       random number to be used for hashing
+     * @return encrypted point
+     */
     GeneralECPoint encrypt(BigInteger message, BigInteger t);
 
-    // Creates Si, and returns Sij to be send to substation.
 }
