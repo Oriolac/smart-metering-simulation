a4b924966f1e2c30291bf094e127936d517e2304
Oriolac <oriolalascercos@gmail.com>
2020-02-17 18:10:49 +0100

    Added all docs of Consumption Package. Needs to be reviewed.

--- src/udl/cig/sms/consumption/ConsumptionReader.java
+++ src/udl/cig/sms/consumption/ConsumptionReader.java
@@ -2,7 +2,16 @@ package udl.cig.sms.consumption;
 
 import java.math.BigInteger;
 
+/**
+ * Extracts dependecy of ConsumptionReader. Used so changing
+ * the reader can be easily done.
+ */
 public interface ConsumptionReader {
 
+    /**
+     * Reads the consumption.
+     *
+     * @return consumption read as a BigInteger
+     */
     BigInteger read();
 }
